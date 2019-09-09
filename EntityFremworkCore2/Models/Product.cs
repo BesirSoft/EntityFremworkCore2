821ae6ae04ac39e9b01c043c2923b5785bb10c26
alieryaman <postauyelik@gmail.com>
2019-09-09 10:28:57 +0300

    .netcore alt yapsı  hazırlandı

---
+++ EntityFremworkCore2/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFremworkCore2.Models
+{
+    public class Product
+    {
+    }
+}
--- EntityFremworkCore2/Startup.cs
+++ EntityFremworkCore2/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EntityFremworkCore2
@@ -13,22 +14,45 @@ namespace EntityFremworkCore2
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+       
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+            public IConfiguration Configuration { get; }
+
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+               
+                    name:"default",
+                    template: "{controller=Product}/{action=List}/{id?}");
             });
+
+
+
+
         }
     }
 }
